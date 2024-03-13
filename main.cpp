a99bdd3799c7b85c923765b71ee2445cdd7bc3ee
Riwaj Mainali <riwaj227@gmail.com>
2024-03-13 17:24:54 -0500

    Initial commit, gotta add lsp and vk-bootstrap

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <vulkan/vulkan.h>
+
+int main() {
+  VkApplicationInfo appInfo = {};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Hello Vulkan";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "No Engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+
+  VkInstance instance;
+  VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+  if (result != VK_SUCCESS) {
+    std::cerr << "Failed to create Vulkan instance!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Vulkan instance created successfully!" << std::endl;
+
+  // Clean up
+  vkDestroyInstance(instance, nullptr);
+
+  return EXIT_SUCCESS;
+}
